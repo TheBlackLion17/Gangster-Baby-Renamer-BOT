d5a43502441a4f152c6f203761961f01784338ed
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-24 00:09:23 +0530

    Error Solved💖

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -28,3 +28,86 @@ async def refunc(client, message):
           f"**Select the output file type**\n**• File Name :-**```{new_name}```",
           reply_to_message_id=file.id,
           reply_markup=InlineKeyboardMarkup(button))
+        
+@Client.on_callback_query(filters.regex("upload"))
+async def doc(bot, update):    
+    new_name = update.message.text
+    new_filename = new_name.split(":-")[1]
+    file_path = f"downloads/{new_filename}"
+    file = update.message.reply_to_message
+
+    ms = await update.message.edit("Tʀyɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅɪɴɢ....")    
+    try:
+     	path = await bot.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram,progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
+    except Exception as e:
+     	return await ms.edit(e)
+     	     
+    duration = 0
+    try:
+        metadata = extractMetadata(createParser(file_path))
+        if metadata.has("duration"):
+           duration = metadata.get('duration').seconds
+    except:
+        pass
+    ph_path = None
+    user_id = int(update.message.chat.id) 
+    media = getattr(file, file.media.value)
+    c_caption = await db.get_caption(update.message.chat.id)
+    c_thumb = await db.get_thumbnail(update.message.chat.id)
+
+    if c_caption:
+         try:
+             caption = c_caption.format(filename=new_filename, filesize=humanbytes(media.file_size), duration=convert(duration))
+         except Exception as e:
+             return await ms.edit(text=f"Yᴏᴜʀ Cᴀᴩᴛɪᴏɴ Eʀʀᴏʀ Exᴄᴇᴩᴛ Kᴇyᴡᴏʀᴅ Aʀɢᴜᴍᴇɴᴛ ●> ({e})")             
+    else:
+         caption = f"**{new_filename}**"
+ 
+    if (media.thumbs or c_thumb):
+         if c_thumb:
+             ph_path = await bot.download_media(c_thumb) 
+         else:
+             ph_path = await bot.download_media(media.thumbs[0].file_id)
+         Image.open(ph_path).convert("RGB").save(ph_path)
+         img = Image.open(ph_path)
+         img.resize((320, 320))
+         img.save(ph_path, "JPEG")
+
+    await ms.edit("Tʀyɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....")
+    type = update.data.split("_")[1]
+    try:
+        if type == "document":
+            await bot.send_document(
+                update.message.chat.id,
+                document=file_path,
+                thumb=ph_path, 
+                caption=caption, 
+                progress=progress_for_pyrogram,
+                progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
+        elif type == "video": 
+            await bot.send_video(
+		update.message.chat.id,
+	        video=file_path,
+	        caption=caption,
+		thumb=ph_path,
+		duration=duration,
+	        progress=progress_for_pyrogram,
+		progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
+        elif type == "audio": 
+            await bot.send_audio(
+		update.message.chat.id,
+		audio=file_path,
+		caption=caption,
+		thumb=ph_path,
+		duration=duration,
+	        progress=progress_for_pyrogram,
+	        progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
+    except Exception as e:          
+        os.remove(file_path)
+        if ph_path:
+            os.remove(ph_path)
+        return await ms.edit(f" Eʀʀᴏʀ {e}")
+ 
+    await ms.delete() 
+    os.remove(file_path) 
+    if ph_path: os.remove(ph_path) 
